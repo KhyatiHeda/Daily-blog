0f94e67641a0b2a2a1842abe4df3f2d7afdffafb
KhyatiHeda <khyati.heda@gmail.com>
2021-05-19 06:44:09 +0530

    m6

--- src/motivation.js
+++ src/motivation.js
@@ -3,8 +3,17 @@ import man1 from './mot-1.jpg';
 import man2 from './mot-2.jpg';
 import man3 from './mot-3.jpg';
 import man4 from './mot-4.jpg';
+import but from './arrow1.png';
+import { useState } from 'react';
 
 const Motivation = () => {
+
+    const[active, setactive]=useState("inactive");
+
+    const HandleClick=()=>
+    {
+        setactive("active");
+    }
     return ( 
        <nav className="motivation">
            <div className="top-container">
@@ -26,6 +35,7 @@ const Motivation = () => {
                <img src={man2} className="pic2" alt="Avatar"></img>
                <img src={man3} className="pic3" alt="Avatar"></img>
                <img src={man4} className="pic4" alt="Avatar"></img>
+               
 
                </div>
                <div><button className="add">+</button></div>
@@ -41,21 +51,26 @@ const Motivation = () => {
                alignItems : 'center'
 
            }}>
-               <div className="All"> All </div>
-               <div className="Article"> Article </div>
-               <div className="Podcast"> Podcast </div>
-               <div className="Video">Video</div>
-               <div className="Downloads"> Downloads </div>
-               <div className="playall"><button className="playall-but">Play all</button></div>
+               <div className="All" Onclick={HandleClick}> All </div>
+               <div className="Article" Onclick={HandleClick}> Article </div>
+               <div className="Podcast" Onclick={HandleClick}> Podcast </div>
+               <div className="Video" Onclick={HandleClick}>Video</div>
+               <div className="Downloads" Onclick={HandleClick}> Downloads </div>
+               <div className="playall" Onclick={HandleClick}><button className="playall-but">Play all</button></div>
            </div>
            </div>
 
            <div className="mid-container">
-               <div className="all1"><p>Your 
+               <div className="all1" style={{
+                   display:'flex',
+                   flexDirection:'column',
+                   justifyContent:'space-around'
+                }}><p>Your 
                                         limitation 
                                         it's only 
                                         your 
-                                        imagination</p></div>
+                                        imagination</p>
+                                        <div className="all1-but"><img src={but} className="all1-img" alt="Avatar"></img></div></div>
                <div className="all2"><p>Great things 
                                           never come from 
                                           comfort zones.</p> </div>
@@ -68,6 +83,9 @@ const Motivation = () => {
                    justifyContent:'space-around'
                 }}><p>Stay focused.</p>
                                        <button className="all4-but">Read Articles </button> </div>
+
+                 <div className="all5"><p> Be creative </p></div>                
+                 <div className="all6"></div>      
            </div>
            
        </nav> 
